d9907fbec2bcda928597c1f89c93849325f12cb6
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2022-12-23 22:11:19 +0200

    DataConstants

---
+++ GiftSystem/Data/DataConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace GiftSystem.Data
+{
+    public class DataConstants
+    {
+        public const int NameMaxLength = 50;
+        public const int NameMinLength = 3;
+        public const int PasswordMaxLength = 20;
+        public const int PasswordMinLength = 5;
+        public const int PhoneNumberMaxLength = 15;
+        public const int PhoneNumberMinLength = 8;
+
+        public const int DescriptionMaxLength = 300;
+    }
+}
