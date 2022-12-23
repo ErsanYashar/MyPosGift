78c440a4ac362dae44a2523f3712d13574232446
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2022-12-23 22:12:07 +0200

    Transaction

---
+++ GiftSystem/Data/Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿namespace GiftSystem.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    using static Data.DataConstants;
+
+    public class Transaction
+    {
+        public int Id { get; init; }
+
+        [Column(TypeName = "decimal(5,2)")]
+        public decimal Amount { get; init; }
+
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; init; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string RecipientName { get; init; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string SenderName { get; init; }
+
+        [Required]
+        public DateTime Date { get; init; }
+    }
+}
