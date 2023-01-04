5eb16fdf9c79e43be06134366c081241635de5eb
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2023-01-04 21:32:57 +0200

    UserRepository

---
+++ GiftSystem/DAL/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace GiftSystem.DAL
+{
+    using System.Collections.Generic;
+    using GiftSystem.Data;
+    using GiftSystem.Data.Models;
+
+    public class UserRepository : IUserRepository
+    {
+        private GiftSystemDbContext context;
+
+        public UserRepository(GiftSystemDbContext context)
+            => this.context = context;
+
+        public ApplicationUser GetUserByPhoneNumber(string phoneNumber)
+            => this.context.Users
+                .FirstOrDefault(u => u.PhoneNumber == phoneNumber);
+
+        public ApplicationUser GetUserById(string userId)
+            => this.context.Users.Find(userId);
+
+        public IEnumerable<ApplicationUser> GetAllUsers()
+            => this.context.Users.ToList();
+    }
+}
